d3ff4a0b09115f826620fa247b9e939a34c435b8
Sciss <contact@sciss.de>
2010-03-09 01:05:31 +0000

    ...

--- DataNucleusTest/enhance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "OBSOLETE !!!! PLEASE USE ant enhance INSTEAD !!!!"
-
-cp resources/package-hsql.orm out/production/DataNucleusTest/de/sciss/datanucleustest
-java -cp out/production/DataNucleusTest:libraries/datanucleus-enhancer.jar:libraries/datanucleus-core.jar:libraries/jdo2-api.jar:libraries/asm.jar org.datanucleus.enhancer.DataNucleusEnhancer out/production/DataNucleusTest/de/sciss/datanucleustest/package-hsql.orm
