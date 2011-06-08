a5cc0d38355b77df35d5f85e63c9aa5cbab1a16a
Sciss <contact@sciss.de>
2011-06-08 17:48:44 +0100

    bump to scala 2.9.0 ; also due to download probs, include netutil directly

--- CupolaRelais/project/build/CupolaRelais.scala
+++ CupolaRelais/project/build/CupolaRelais.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class CupolaRelaisProject( info: ProjectInfo ) extends DefaultProject( info ) {
-   val dep1 = "de.sciss" % "netutil" % "0.38" from "http://github.com/downloads/Sciss/Cupola/netutil-0.38.jar"
+//   val dep1 = "de.sciss" % "netutil" % "0.38" from "http://github.com/downloads/Sciss/Cupola/netutil-0.38.jar"
 }
