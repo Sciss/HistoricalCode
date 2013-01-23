698f75449d33a86a02328d2f33749a9da5652b2f
Sciss <contact@sciss.de>
2013-01-23 14:44:57 +0100

    look more into it; bug reproducible with v0.0.1; perhaps should enable the tails()== assertion in append-finish?

--- ContextSnake/src/test/scala/de/sciss/contextsnake/ContextTreeSuite.scala
+++ ContextSnake/src/test/scala/de/sciss/contextsnake/ContextTreeSuite.scala
@@ -3,16 +3,37 @@ package de.sciss.contextsnake
 import org.scalatest.FunSuite
 
 class ContextTreeSuite extends FunSuite {
-  val seed  = 0L
-  val N     = 8 // 10000 // corpus size. higher than 10k gets slow because of the `.tails` iterations
-  val M     = 2  // 26    // maximum number of different symbols
+  val BROKEN  = true
+
+  val seed    = 0L
+  val N       = 8 // 10000 // corpus size. higher than 10k gets slow because of the `.tails` iterations
+  val M       = 2  // 26    // maximum number of different symbols
 
   test("sub-sequences searches are performed on the suffix tree") {
     val r   = new util.Random(seed)
     val v   = Vector.fill(N)(r.nextInt(M))
-    val c   = ContextTree(v: _*)
-    val t1  = v.tails.forall(c.containsSlice)
-    assert(t1)
+    val c   = ContextTree.empty[Int]
+
+    if (BROKEN) {
+      println("INPUT: " + v)
+      c.appendAll(v.init)
+      println("GRAPH with N=" + (N-1))
+      println(c.toDOT(tailEdges = true, sep=","))
+      println("...appending " + v.last)
+      c.append(v.last)
+      println("\nGRAPH with N=" + N)
+      println(c.toDOT(tailEdges = true, sep=","))
+      v.tails.foreach { sq =>
+        if (!c.containsSlice(sq)) {
+          println("DOES NOT FIND: " + sq)
+        }
+      }
+
+    } else {
+      c.appendAll(v)
+      val t1  = v.tails.forall(c.containsSlice)
+      assert(t1)
+    }
 
     assert(c.size === v.size)
     val t2  = v.tails.filter(_.nonEmpty).map { xs =>
