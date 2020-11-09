53333720c836a48c7f3676296d8a9fdd7ddc7a28
Hanns Holger Rutz <contact@sciss.de>
2020-11-09 21:44:17 +0100

    first commit

---
+++ DottyLucrePatMat/src/main/scala/de/sciss/lucre/Ident.scala
@@ -0,0 +1,17 @@
+/*
+ *  Ident.scala
+ *  (Lucre 4)
+ *
+ *  Copyright (c) 2009-2020 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is published under the GNU Affero General Public License v3+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.lucre
+
+trait Ident[T <: Exec[T]] {
+}
